54d89cdc26d8344686eb4cfbcc1fc9ed97f89b0f
Kanjina <pengsenfang99@gmail.com>
2026-04-05 18:31:40 -0700

    Update Catch_backend.py

--- Catch_backend.py
+++ Catch_backend.py
@@ -1,2 +1,2 @@
 social_media_screen_time = 0 #social media time ammount variable(inputdata)
-device_on = False #Boolean variable to detect wether or not hte phone is on or off(inputdata)
+device_on = False #Boolean variable to detect whether or not hte phone is on or off(inputdata)
